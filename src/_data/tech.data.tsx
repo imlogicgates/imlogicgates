44c684f402c620a581271095b615364694741198
Jig James Marababol <jjbmarababol@gmail.com>
2025-03-21 08:37:11 +0800

    [IMLG-1] Tech Logos Update

--- src/_data/tech.data.tsx
+++ src/_data/tech.data.tsx
@@ -25,6 +25,11 @@ export const techLogos = [
     url: "https://www.testdome.com/certificates/e0be89e891f54e08b5c830f150e48aad",
   },
   {
+    image: "github.svg",
+    alt: "GitHub",
+    url: "https://www.testdome.com/certificates/e6b1ad7129e540b99fc0d08aa1f5b3ca",
+  },
+  {
     image: "react.svg",
     alt: "React",
     url: "https://www.testdome.com/certificates/546ca9afa9104755b51e26ec059c0bb7",
@@ -38,11 +43,6 @@ export const techLogos = [
     image: "sql.svg",
     alt: "SQL",
     url: "https://www.testdome.com/certificates/db6daa553f4a460b8d53b88da4b1baf8",
-  },
-  {
-    image: "github.svg",
-    alt: "GitHub",
-    url: "https://www.testdome.com/certificates/e6b1ad7129e540b99fc0d08aa1f5b3ca",
   },
   {
     image: "api.svg",
